9257b6c0586fb0c5c685b5ea0168786e37dde3f8
Anastasia <anastasia.ukhlina@gmail.com>
2024-05-11 15:51:01 +0200

    js: input-form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-'use strict';
-console.log('prova');
+'use strict'; //?
 
-const h2 = document.getElementById('idprova');
-console.log(h2);
+//input form
+const userKM = document.getElementById('travel-KM');
+console.log(userKM);
+
+//range input con id:user-years
+const userAge = document.getElementById('user-years');
+console.log(userAge);
+
+// const h2 = document.getElementById('idprova');
+// console.log(h2);
