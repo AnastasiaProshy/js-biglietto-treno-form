90ef393e9a0f4b6b1fa3b2b22b5d0a304f2b1acf
Anastasia <anastasia.ukhlina@gmail.com>
2024-05-11 19:38:47 +0200

    js: add-event-listener

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
 'use strict'; //?
 
+// Leggo il Form
+const formTicket = document.getElementById('ticket');
+console.log(formTicket);
+
+
+
 //input form
 const userKM = document.getElementById('travel-KM');
 console.log(userKM);
 
-//range input con id:user-years
+
+
+// Range input con id: user-years
 const userAge = document.getElementById('user-years');
 console.log(userAge);
 
-// const h2 = document.getElementById('idprova');
-// console.log(h2);
+//input current-age
+const currentAge = document.querySelector('.current-age');
+console.log(currentAge);
+
+//imposto il valore iniziale visualizzato di anni
+currentAge.innerText = userAge.value;
+
+//cambia il valore corrente di innerText
+userAge.addEventListener('change', function(){
+    currentAge.innerText = userAge.value;
+})
+ 
+
+
+
+
+
+
+//Invio del Form
+formTicket.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    console.log('ho inviato il form');
+});
+
+
+
