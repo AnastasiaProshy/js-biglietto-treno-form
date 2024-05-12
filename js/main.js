7881ffed7c99a84fd20220f96cf8ab96b6ad4d6e
Anastasia <anastasia.ukhlina@gmail.com>
2024-05-12 22:12:05 +0200

    js: new-calc-module

--- js/main.js
+++ js/main.js
@@ -4,13 +4,9 @@
 const formTicket = document.getElementById('ticket');
 console.log(formTicket);
 
-
-
 //input form
 const userKM = document.getElementById('travel-KM');
 console.log(userKM);
-
-
 
 // Range input con id: user-years
 const userAge = document.getElementById('user-years');
@@ -28,29 +24,51 @@ userAge.addEventListener('change', function () {
     currentAge.innerText = userAge.value;
 })
 
-
-
-
-
-//Invio del Form is other program
+//Invio del Form is other program*************************
 formTicket.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const userKM = document.getElementById('travel-KM');
-    const userAge = document.getElementById('user-years');
+    const userAGE = document.getElementById('user-years');
 
     console.log(userKM.value);
-    console.log(userAge.value);
+    console.log(userAGE.value);
 
-    //operazioni di calcolo
+    // Dichiarazione della variabile finalResult
+    let finalResult;
 
+    //operazioni di calcolo*******************************************************************************************
 
-    //risultato del calcolo
-    document.getElementById('Result').innerText = 'y';
+    //MAIN ALGORITM
+    const DefaultPrice = (userKM.value * 0.21).toFixed(2);
+    const priceUnder18 = (DefaultPrice - (DefaultPrice * 0.20)).toFixed(2);
+    const priceOver65 = (DefaultPrice - (DefaultPrice * 0.40)).toFixed(2);
+
+    //OUTPUT
+    const string = `The total price of your trip is `
+
+    if (userAGE.value <= 18) {
+        finalResult = string + priceUnder18 + `€. Congratulations, you've received a 20% discount for under 19 years.`;
+        console.log(priceUnder18);
+    }
+    else if (userAGE.value >= 65) {
+        finalResult = string + priceOver65 + `€. Congratulations, you've received a 40% discount for over 64 years.`;
+        console.log(priceOver65);
+    }
+    else {
+        finalResult = string + DefaultPrice + `€. Unfortunately, there are no discounts available for you.`;
+        console.log(DefaultPrice);
+    }
+
+    //risultato del calcolo********************************************************************************************
+    document.getElementById('Result').innerText = finalResult;
     document.getElementById('fruit-box').classList.remove('d-none');
-
 
 });
 
-
-
+//bottone reset
+document.getElementById('reset').addEventListener('click', function () {
+    document.getElementById('Result').innerText = ''; //svuota campo di testo
+    document.getElementById('fruit-box').classList.add('d-none'); //elimina contenitore di resultato
+    document.querySelector('.current-age').innerText = '';
+});
