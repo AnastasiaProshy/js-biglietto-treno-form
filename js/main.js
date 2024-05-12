766bc8be8bd406bf91aedc5f1d98e72d25746d38
Anastasia <anastasia.ukhlina@gmail.com>
2024-05-12 22:20:10 +0200

    update: output

--- js/main.js
+++ js/main.js
@@ -12,17 +12,7 @@ console.log(userKM);
 const userAge = document.getElementById('user-years');
 console.log(userAge);
 
-//input current-age
-const currentAge = document.querySelector('.current-age');
-console.log(currentAge);
-
-//imposto il valore iniziale visualizzato di anni
-currentAge.innerText = userAge.value;
-
-//cambia il valore corrente di innerText
-userAge.addEventListener('change', function () {
-    currentAge.innerText = userAge.value;
-})
+let finalResult;
 
 //Invio del Form is other program*************************
 formTicket.addEventListener('submit', function (event) {
@@ -31,34 +21,27 @@ formTicket.addEventListener('submit', function (event) {
     const userKM = document.getElementById('travel-KM');
     const userAGE = document.getElementById('user-years');
 
+    const DefaultPrice = (userKM.value * 0.21).toFixed(2);
+
+    //OUTPUT
+    const string = `The total price of your trip is € `
+
+    if (userAGE.value == 2) {
+        finalResult = string + (DefaultPrice - (DefaultPrice * 0.20)).toFixed(2);;
+        console.log(userKM);
+    }
+    else if (userAGE.value == 3) {
+        finalResult = string + (DefaultPrice - (DefaultPrice * 0.40)).toFixed(2);
+        console.log(userKM);
+    }
+    else {
+        finalResult = string + DefaultPrice;
+        console.log(userKM);
+    }
+
     console.log(userKM.value);
     console.log(userAGE.value);
 
-    // Dichiarazione della variabile finalResult
-    let finalResult;
-
-    //operazioni di calcolo*******************************************************************************************
-
-    //MAIN ALGORITM
-    const DefaultPrice = (userKM.value * 0.21).toFixed(2);
-    const priceUnder18 = (DefaultPrice - (DefaultPrice * 0.20)).toFixed(2);
-    const priceOver65 = (DefaultPrice - (DefaultPrice * 0.40)).toFixed(2);
-
-    //OUTPUT
-    const string = `The total price of your trip is `
-
-    if (userAGE.value <= 18) {
-        finalResult = string + priceUnder18 + `€. Congratulations, you've received a 20% discount for under 19 years.`;
-        console.log(priceUnder18);
-    }
-    else if (userAGE.value >= 65) {
-        finalResult = string + priceOver65 + `€. Congratulations, you've received a 40% discount for over 64 years.`;
-        console.log(priceOver65);
-    }
-    else {
-        finalResult = string + DefaultPrice + `€. Unfortunately, there are no discounts available for you.`;
-        console.log(DefaultPrice);
-    }
 
     //risultato del calcolo********************************************************************************************
     document.getElementById('Result').innerText = finalResult;
@@ -72,3 +55,5 @@ document.getElementById('reset').addEventListener('click', function () {
     document.getElementById('fruit-box').classList.add('d-none'); //elimina contenitore di resultato
     document.querySelector('.current-age').innerText = '';
 });
+
+
